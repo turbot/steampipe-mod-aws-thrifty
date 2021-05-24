6c16afebb71a700c783f9e8de9bb3c40313aac17
David Boeke <david@boeke.com>
2021-05-24 19:32:39 -0400

    add rds checks

---
+++ query/old_rds_db_instances.sql
@@ -0,0 +1,12 @@
+select
+  arn as resource,
+  case
+    when date_part('day', now()-create_time) > 90 then 'alarm'
+    when date_part('day', now()-create_time) > 30 then 'info'
+    else 'ok'
+  end as status,
+  title || ' has been in use for ' || date_part('day', now()-create_time) || ' days.' as reason,
+  region,
+  account_id
+from
+  aws_rds_db_instance
