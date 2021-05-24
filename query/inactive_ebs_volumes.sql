ba66a964fc7b2941518fe9cd1dbf47f12ace9cd2
David Boeke <david@boeke.com>
2021-05-24 17:51:32 -0400

    change query to group by logic

--- query/inactive_ebs_volumes.sql
+++ query/inactive_ebs_volumes.sql
@@ -7,30 +7,31 @@ select
   volume_id || ' is attached to ' || running_instances || ' running instances.' as reason,
   region,
   account_id
-from (
-  select
-    v.arn,
-    v.volume_id,
-    i.instance_id,
-    v.region,
-    v.account_id,
-    sum(
-      case 
-        when i.instance_state = 'stopped' then 0
-        else 1
-      end
-    ) as running_instances
-  from
-    aws_ebs_volume v,
-    jsonb_array_elements(v.attachments) va, 
-    aws_ec2_instance i
-  where
-    va ->> 'InstanceId' = i.instance_id
-  group by
-    v.arn, 
-    v.volume_id,
-    i.instance_id,
-    i.instance_id,
-    v.region,
-    v.account_id
-) as ebs_volumes
+from 
+  (
+    select
+      v.arn,
+      v.volume_id,
+      i.instance_id,
+      v.region,
+      v.account_id,
+      sum(
+        case 
+          when i.instance_state = 'stopped' then 0
+          else 1
+        end
+      ) as running_instances
+    from
+      aws_ebs_volume v,
+      jsonb_array_elements(v.attachments) va, 
+      aws_ec2_instance i
+    where
+      va ->> 'InstanceId' = i.instance_id
+    group by
+      v.arn, 
+      v.volume_id,
+      i.instance_id,
+      i.instance_id,
+      v.region,
+      v.account_id
+  ) as ebs_volumes;
