a420986c59dbad3a721f1aabef87f686f56eb99e
David Boeke <david@boeke.com>
2021-05-24 13:28:44 -0400

    Iops checks closes #4 (#5)
    * low and high iops checks
    
    * add newline to end of files

---
+++ query/high_iops_volumes.sql
@@ -0,0 +1,15 @@
+select
+  arn as resource,
+  case
+    when volume_type not in ('io1', 'io2') then 'skip'
+    when iops >32000 then 'alarm'
+    else 'ok'
+  end as status,
+  case
+    when volume_type not in ('io1', 'io2') then volume_id || ' type is ' || volume_type || '.'
+    else volume_id || ' has ' || iops || ' iops.'
+  end as reason,
+  region,
+  account_id
+from
+  aws_ebs_volume;
