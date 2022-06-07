236d635b81309e8d7b47467d7b985a12d9be359d
souravturbot <sourav@turbot.com>
2022-06-07 21:58:06 +0530

    Add control full_month_forecast_cost_changes

---
+++ query/costexplorer/monthly_forecast_cost_changes.sql
@@ -0,0 +1,44 @@
+with prev_month as (
+    select
+      sum(net_unblended_cost_amount) as cost,
+      partition,
+      account_id
+    from
+      aws_cost_usage
+    where
+      granularity = 'MONTHLY'
+      and dimension_type_1 = 'SERVICE'
+      and dimension_type_2 = 'RECORD_TYPE'
+      and dimension_2 not in ('Credit')
+      and period_start >= date_trunc('month', current_date - interval '1' month)
+      and period_start < date_trunc('month', current_date )
+    group by
+      account_id,partition
+  ),
+  forecast_month as (
+    select
+      mean_value as cost,
+      partition,
+      account_id
+    from
+      aws_cost_forecast_monthly
+    order by
+      period_start limit 1
+  )
+select
+  'arn:' || prev_month.partition || ':::' || prev_month.account_id as resource,
+  case
+    when forecast_month.cost > prev_month.cost then 'alarm'
+    else 'ok'
+  end as status,
+  case
+    when forecast_month.cost > prev_month.cost then prev_month.account_id || ' forecasted monthly cost has increased by ' || round(cast((forecast_month.cost - prev_month.cost) as numeric), 2) || '.'
+    when forecast_month.cost < prev_month.cost then prev_month.account_id || ' forecasted monthly cost has decreased by ' || round(cast((prev_month.cost - forecast_month.cost) as numeric), 2) || '.'
+    else prev_month.account_id || ' forecasted monthly cost has remained flat.'
+  end as reason,
+  prev_month.account_id
+from
+  prev_month,
+  forecast_month
+where
+  forecast_month.account_id = prev_month.account_id;
--- thrifty/cost_variance.sp
+++ thrifty/cost_variance.sp
@@ -15,7 +15,8 @@ benchmark "cost_variance" {
   description   = "."
   documentation = file("./thrifty/docs/cost_variance.md")
   children = [
-    control.full_month_cost_changes
+    control.full_month_cost_changes,
+    control.full_month_forecast_cost_changes
   ]
 
   tags = merge(local.cost_variance_common_tags, {
@@ -39,3 +40,12 @@ control "full_month_cost_changes" {
   })
 }
 
+control "full_month_forecast_cost_changes" {
+  title       = "What is the forecasted monthly cost over last month’s cost?"
+  description = "Compares the monthly cost of forecast with last month’s cost."
+  sql         = query.monthly_forecast_cost_changes.sql
+  severity    = "low"
+  tags = merge(local.cost_variance_common_tags, {
+    service = "AWS/CostExplorer"
+  })
+}
